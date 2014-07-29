ece1a85ed32dff206869d42e989e6d9857864ba4
Daniel Norman <daniel.norman@sociomantic.com>
2014-07-29 20:15:51 +0200

    add a new test log file and implement cut by bytes

--- cut.d
+++ cut.d
@@ -1,153 +1,317 @@
 import std.stdio;
 import std.getopt;
 import std.conv;
+import std.file;
+import std.algorithm;
 
-immutable char hyphen = 0x2D;
 
-/**
- * struct range
- * 
- * Data structure representing a range
- */
-struct range {
-    uint from;
-    uint to;
-}
+class Cut {
 
-/**
- * struct options
- * 
- * Data structure representing the cli options
- */
-struct options {
+    /**
+     * Range
+     *
+     * Range data structure representing a range of fields/bytes
+     */
+    struct Range {
+        uint from;
+        uint to;
+    }
+
+    /**
+     * Cut Enumerated Type
+     *
+     * Data structure representing the different modes cut can operate in
+     */
+    enum CutMode { fields, bytes }
+
+
+    /**
+     * Constant chars
+     *
+     * Used for easy reference
+     */
+    immutable static char hyphen = 0x2D;
+    immutable static char tab = 0x09;
+    immutable static char newLine = 0x0A;
+
+
+
+    /**
+     * @var file the file to read
+     */
     string file;
-    range fields;
-    range bytes;
-    char delimiter = 0x09;
+
+    /**
+     * @var mode    cut mode
+     */
+    CutMode mode;
+
+    /**
+     * @var range   range of fields/bytes selected
+     */
+    Range range;
+
+    /**
+     * @var delimiter   the character used as delimiter.
+     */
+    char delimiter = tab;
     bool complement = false;
-}
 
-void main(string[] args) {
-    options opts;
-
-    parseOpts(args, opts);
-
-    writefln("option: %s ", opts);
-
-}
-
-/**
- * Parse Options
- * 
- * Parses the cli options and updates the options data structure
- * 
- * @param   args    string[]    arguments as passed to the main function
- * @param   &opts   options     the options data strcuture to update
- * 
- * @return  void
- */
-void parseOpts(string[] args, ref options opts) {
-    string fields;
-    string bytes;
-
-    getopt(args, 
-        std.getopt.config.passThrough,
-        "complement", &opts.complement,
-        "delimiter|d", &opts.delimiter,
-        "bytes|b", &bytes,
-        "fields|f", &fields);
-
-    if (bytes.length == 0 && fields.length == 0)
-    {
-        writeln("usage: cut -b list [file ...]\n cut -f list [-d delim] [file ...]");
-        writeln("specify either a byte or a field range ");
-        return;        
+    this(string[] args) {
+        parseOpts(args);
+        this();
     }
 
-    parseRange(fields, opts.fields);
-
-    if (args.length == 2) {
-        opts.file = args[1];
-    } else {
-        writeln("missing the file option.");
-        return;
-    }
-}
 
 
-
-/**
- * Parse Range
- * 
- * Parses a range string and updates the range data structure
- * 
- * @param   input   string      the string entered by the user
- * @param   range   range       the range data structure to update
- * 
- * @return  void
- */
-void parseRange(string input, ref range range) {
-    bool hasHyphen = false;
-
-    foreach(i, e; input) {
-        if (e == hyphen) {
-            hasHyphen = true;
-            if (i == 0) {
-                if(input.length == 1) {
-                    // exception
-                    return;
-                }
-                range.from = 1;
-                range.to = to!uint(input[i+1 .. $]);
-                break;
-            } else if (i == input.length -1) {
-                range.from = to!uint(input[0 .. i]);
-                range.to = uint.max;
-                break;
-            } else {
-                range.from = to!uint(input[0 .. i]);
-                range.to = to!uint(input[i+1 .. $]);
-                break;
-            }
+    this() {
+        if (!exists(file)) {
+            writefln("file %s doesn't exists", file);
+            return;
+        } else {
+            readFile();
         }
     }
 
-    // single field
-    if (!hasHyphen) {
-        range.from = to!uint(input);
-        range.to = to!uint(input);
+
+    void readFile() {
+        auto f = File(file, "r");
+        string output;
+
+        char[] buf;
+
+
+        while (f.readln(buf)) {
+            //writeln(cutLine(buf.filter!(a => a != newLine)));
+            writeln(cutLine(buf.strip(' ')));
+        }
     }
 
-    if(range.from == 0 || range.to == 0 ) {
-        // exception
+    auto cutLine(char[] line) {
+        if (mode == CutMode.fields) {
+            return cutLineByFields(line);
+        } else {
+            return cutLineByBytes(line);
+        }
     }
 
+
+    /**
+     * Cut lines based on fields
+     *
+     * Splits the line acording to the delimiter and returns the fields
+     *
+     * @param   char[]  line    a single line
+     *
+     * @return  char[]
+     */
+    auto cutLineByFields(char[] line) {
+        auto fields = splitter(line, delimiter);
+        char[] result;
+
+        auto c = 1;
+        foreach (field; fields) {
+            // stop iterating if the end field has been reached.
+            if (c > range.to) {
+                break;
+            }
+
+            // Append to the result the delimiter which has been removed by the splitter.
+            if (c >= range.from && c <= range.to) {
+                result ~= field ~ delimiter;
+            }
+
+            c++;
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Cut lines based on bytes
+     *
+     * @param   char[]  line    a single line
+     *
+     * @return  char[]
+     */
+    auto cutLineByBytes(char[] line) {
+        char[] result;
+
+        auto c = 1;
+        foreach (field; line) {
+            // stop iterating if the end field has been reached.
+            if (c > range.to) {
+                break;
+            }
+
+            // Append to the result the delimiter which has been removed by the splitter.
+            if (c >= range.from && c <= range.to) {
+                result ~= field;
+            }
+
+            c++;
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Parse Options
+     *
+     * Parses the cli options and updates the options data structure
+     *
+     * @param   args    string[]    arguments as passed to the main function
+     *
+     * @return  void
+     */
+    void parseOpts(string[] args) {
+        string fields;
+        string bytes;
+
+        getopt(args,
+            std.getopt.config.passThrough,
+            "complement", &this.complement,
+            "delimiter|d", &this.delimiter,
+            "bytes|b", &bytes,
+            "fields|f", &fields);
+
+
+        if ((bytes.length == 0 && fields.length == 0) ||
+            (bytes.length > 0 && fields.length > 0))
+        {
+            writeln("usage: cut -b list [file ...]\n cut -f list [-d delim] [file ...]");
+            writeln("specify either a byte or a field range ");
+            return;
+        }
+
+        this.mode = (fields.length > 0) ? CutMode.fields : CutMode.bytes;
+
+        if(this.mode == CutMode.fields) {
+            parseRange(fields);
+        } else {
+            parseRange(bytes);
+        }
+
+
+        if (args.length == 2) {
+            this.file = args[1];
+        } else {
+            writeln("missing the file option.");
+            return;
+        }
+    }
+
+
+
+    /**
+     * Parse Range
+     *
+     * Parses a range string and updates the range data structure
+     *
+     * @param   input   string      the string entered by the user
+     * @param   range   range       the range data structure to update
+     *
+     * @return  void
+     */
+    void parseRange(string input) {
+        bool hasHyphen = false;
+
+        foreach(i, e; input) {
+            if (e == hyphen) {
+                hasHyphen = true;
+                if (i == 0) {
+                    if(input.length == 1) {
+                        // exception
+                        return;
+                    }
+                    range.from = 1;
+                    range.to = to!uint(input[i+1 .. $]);
+                    break;
+                } else if (i == input.length -1) {
+                    range.from = to!uint(input[0 .. i]);
+                    range.to = uint.max;
+                    break;
+                } else {
+                    range.from = to!uint(input[0 .. i]);
+                    range.to = to!uint(input[i+1 .. $]);
+                    break;
+                }
+            }
+        }
+
+        // single field
+        if (!hasHyphen) {
+            range.from = to!uint(input);
+            range.to = to!uint(input);
+        }
+
+        if(range.from == 0 || range.to == 0 ) {
+            // exception
+        }
+
+
+    }
 
 }
 
+// Main function
+void main(string[] args) {
+    auto cut = new Cut(args);
+}
 
+
+// Unit test for parse range
 unittest {
-    range range;
+    auto cut = new Cut();
 
-    parseRange("1", range);
-    assert(range.from == 1 && range.to == 1);
+    cut.parseRange("1");
+    assert(cut.range.from == 1 && cut.range.to == 1);
 
-    parseRange("1-", range);
-    assert(range.from == 1 && range.to == uint.max);
-    
-    parseRange("1-5", range);
-    assert(range.from == 1 && range.to == 5);    
- 
-    parseRange("-5", range);
-    assert(range.from == 1 && range.to == 5); 
+    cut.parseRange("1-");
+    assert(cut.range.from == 1 && cut.range.to == uint.max);
 
-    range.from = 0;
-    range.to = 0;
-    parseRange("-", range);
-    assert(range.from == 0 && range.to == 0);
+    cut.parseRange("1-5");
+    assert(cut.range.from == 1 && cut.range.to == 5);
+
+    cut.parseRange("-5");
+    assert(cut.range.from == 1 && cut.range.to == 5);
+
+    cut.range.from = 0;
+    cut.range.to = 0;
+    cut.parseRange("-");
+    assert(cut.range.from == 0 && cut.range.to == 0);
 
 
-    parseRange("1-1", range);
-    assert(range.from == 1 && range.to == 1);
+    cut.parseRange("1-1");
+    assert(cut.range.from == 1 && cut.range.to == 1);
+}
+
+
+
+
+// Unit test for cut lines by fields/bytes
+unittest {
+    auto cut = new Cut();
+    cut.range.from = 1;
+    cut.range.to = 5;
+    cut.delimiter = 0x20;
+
+    char[] test1 = "col1 col2 col3 col4 col5 col6 col7 col8".dup;
+
+    assert(cut.cutLineByFields(test1) == "col1 col2 col3 col4 col5 ");
+
+    cut.range.from = 1;
+    cut.range.to = uint.max;
+
+    assert(cut.cutLineByFields(test1) == "col1 col2 col3 col4 col5 col6 col7 col8 ");
+
+
+    cut.range.from = 1;
+    cut.range.to = 4;
+
+    assert(cut.cutLineByBytes(test1) == "col1");
+
 
 }
